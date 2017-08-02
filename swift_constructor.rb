85a33296f7d687754a5445754a1f99d8e7846bf6
Theo Massard <massar_t@etna-alternance.net>
2017-08-02 16:23:16 +0200

    Lint strings and stuff

--- swift_constructor.rb
+++ swift_constructor.rb
@@ -1,19 +1,21 @@
+#!/usr/bin/env ruby
 def read_file(file_name)
-  file = File.open(file_name, "r")
+  file = File.open(file_name, 'r')
   data = file.read
   file.close
-  return data
+  data
 end
 
-constructPrototype = ""
-constructContent = ""
+construct_prototype = ''
+construct_content = ''
 read_file(ARGV[0]).scan(/var ((.*):.*)/).each do |match|
-  constructPrototype += match[0] + ", "
-  constructContent += "\tself." + match[1] + " = " + match[1] + "\n"
+  construct_prototype += match[0] + ', '
+  construct_content += '\tself.' + match[1] + ' = ' + match[1] + '\n'
 end
-constructPrototype = constructPrototype[0...-2]
-constructContent = constructContent[0...-1]
-result = "init(#{constructPrototype}){\n#{constructContent}\n}"
+
+construct_prototype = construct_prototype[0...-2]
+construct_content = construct_content[0...-1]
+result = "init(#{construct_prototype}){\n#{construct_content}\n}"
 puts result
 open(ARGV[0], 'a') do |f|
   f << result
